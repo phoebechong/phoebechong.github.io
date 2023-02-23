bdcf1f7b4b558cb14af64d663a4477277fb5b9fc
phoebechong <bobaechong@gmail.com>
2023-02-23 09:14:05 -0500

    Revert "quickfit edit"
    This reverts commit 4b4f57d80a7725b13028beca54e8e25f97282ef2.

--- text_resize.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-$(function() {
-    $(window).on('resize', function() {
-      $('#quickfit-a').quickfit({ max: 40, min: 20, truncate: false });
-      $('#quickfit-b').quickfit({ max: 40, min: 20, truncate: true });        
-      $('#quickfit-c').quickfit({ max: 40, min: 20, truncate: true });
-    });
-    $(window).trigger('resize');    
-</script>
